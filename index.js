6f27ab8e07a958bb48dcfd6c69a1ca522787d244
Jahangir-Alam-Noman <jahangir2km@gmail>
2021-11-11 15:47:22 +0600

    purchase updated

--- index.js
+++ index.js
@@ -13,16 +13,35 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.veusr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
-console.log(uri);
-
 async function run() {
     try {
         await client.connect();
-        const database = client.db("insertDB");
-        const haiku = database.collection("haiku");
+        const database = client.db("product_sales");
+        const productsCollection = database.collection("products");
 
-        console.log('database connected successfully');
 
+        // get api for all products for home and explore page
+        app.get('/products', async (req, res) => {
+            const cursor = productsCollection.find({});
+            const result = await cursor.toArray();
+            res.send(result);
+        })
+
+        // get  api for single product for purchase page
+        app.get('/products/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const product = await productsCollection.findOne(query);
+            res.json(product);
+        })
+
+
+        // post api for adding product
+        app.post('/products', async (req, res) => {
+            const product = req.body;
+            const result = await productsCollection.insertOne(product);
+            res.send(result);
+        })
 
 
 
@@ -37,7 +56,7 @@ run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
-    res.send('Hello product sales!')
+    res.send('Product sales!')
 })
 
 app.listen(port, () => {
